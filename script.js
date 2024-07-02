39a2bbbe7d13acf4afc983f9ad662eefc3228314
satorwxz <bekakasymbekov23@gmail.com>
2024-07-02 13:49:27 +0600

    add lesson 17

---
+++ script.js
@@ -0,0 +1,26 @@
+//let a=4//
+//let b=55//
+//console.log(a + b)//
+//console.log(a - b)//
+//console.log(a * b)//
+//console.log(a / b)//
+
+//let a='hello,'//
+//let b='Bektur'//
+//console.log(a + b)//
+
+//let a='Hi'//
+//let b=5//
+//console.log(a / b)//
+
+//let a=5//
+//let b='5'//
+//console.log(a + 5)//
+
+
+let c=2
+let b
+console.log(c == b)
+//Выходит False,потому что 2 не равняется к undefined//
+console.log(c === b)
+//Выходит False,потому что 2 не равняется к undefined//
